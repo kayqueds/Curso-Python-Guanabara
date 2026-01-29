c82ce57740fa06fc70e4ce12b44646b842ff361a
Kayque Estevão <kayque2911@gmail.com>
2026-01-29 19:52:04 -0300

    feat: organizando e criando diretórios Testes e Desafios, fazendo aula6

---
+++ Modulo_01/Testes/aula06a.py
@@ -0,0 +1,6 @@
+# Tipos primitivos de dados
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1+n2
+# usando o método format
+print('A soma entre {} e {} vale: {}'.format(n1,n2,s))
---
+++ Modulo_01/Testes/aula06b.py
@@ -0,0 +1,4 @@
+valor = float(input('Digite um valor: '))
+tipo_dado = type(valor)
+
+print("O {} é do tipo {}".format(valor, tipo_dado))
